aa307c6d6868edeea84144d843fc5877a03023cc
Soki <soki.fin02@gmail.com>
2024-05-11 23:28:35 +0300

    lisätty testi game_turn:lle

--- src/tests/game_turn_test.py
+++ src/tests/game_turn_test.py
@@ -15,6 +15,14 @@ class TestDice(unittest.TestCase):
         for dice, val in zip(self.turn.dices, test_values):
             self.assertEqual(dice.value, val)
 
+    def test_roll_dices_locked(self):
+        random.seed(1)
+        for i in range(5):
+            self.turn.dices[i].locked = True
+        self.turn.throw_dices()
+        test_values = [0, 0, 0, 0, 0]
+        for dice, val in zip(self.turn.dices, test_values):
+            self.assertEqual(dice.value, val)
 
 if __name__ == "__main__":
     unittest.main()
